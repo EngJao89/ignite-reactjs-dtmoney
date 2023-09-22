af56b41c5390970d1e8d9af4917f51208f4eab41
EngJao89 <eng_joao@outlook.com>
2023-09-22 10:56:09 -0300

    feat(dm03-summary): implementa card com resumo de transações

---
+++ src/Components/Summary/index.tsx
@@ -0,0 +1,35 @@
+import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from "phosphor-react";
+import { SummaryCard, SummaryContainer } from "./styles";
+
+export function Summary() {
+  return(
+    <SummaryContainer>
+      <SummaryCard>
+        <header>
+          <span>Entrada</span>
+          <ArrowCircleUp size={32} color="#00b37e" />
+
+          <strong>R$ 17.400,00</strong>
+        </header>
+      </SummaryCard>
+
+      <SummaryCard>
+        <header>
+          <span>Saída</span>
+          <ArrowCircleDown size={32} color="#f75a68"/>
+
+          <strong>R$ 1.259,00</strong>
+        </header>
+      </SummaryCard>
+
+      <SummaryCard variant="green">
+        <header>
+          <span>Total</span>
+          <CurrencyDollar size={32} color="#fff" />
+
+          <strong>R$ 16.141,00</strong>
+        </header>
+      </SummaryCard>
+    </SummaryContainer>
+  )
+}
